bd991daaf4a036b90ab7eccb83fb1190fbcb02ca
Allan <allan.duhe@edu.devinci.fr>
2021-06-10 21:54:45 +0200

    add db

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
+import Library from './components/Library';
+
+import { useState, useEffect } from 'react';
+
+type Videos = {
+  title: string,
+  id: string
+}
+
+export type User = {
+  name: string,
+  videos: Videos[],
+  libName: string
+}
 function App() {
+
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    fetch("http://localhost:8400", {
+      method: 'GET'
+    })
+      .then(response => response.json())
+      .then(data => setUser(data))
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      {user ? <Library user={user} /> : <h1>Chargement</h1>}
+      {/* Library component which displays the lib name + videos in the lib */}
+      <div className="SearchAndDisplay">
+
+      </div>
     </div>
   );
 }
--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -1,5 +1,29 @@
-function Library() {
+import { useState } from 'react';
 
+import { User } from '../App';
+
+type LibraryProps = {
+    user: User
+}
+
+function Library({ user }: LibraryProps) {
+
+    const [searchOrVideo, setSearchOrVideo] = useState('');
+    console.log(user.libName);
+
+    return (
+        <div className="lib_container">
+            <div className="lib_name_container">
+                <h1>{user.libName}</h1>
+                <div className="displaySearch">
+                    <button onClick={() => { setSearchOrVideo("Search") }}>Ajouter une vidéo à ma librairie</button>
+                </div>
+                <div className="Videos">
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Library;
